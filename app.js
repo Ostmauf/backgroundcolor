8501f64d08f77361c8f0692cedd60e1143588442
Ostmauf <71626781+Ostmauf@users.noreply.github.com>
2021-08-17 12:01:00 +0300

    to rgb

---
+++ app.js
@@ -0,0 +1,7 @@
+const usrInput1 = document.getElementById("usr-input-1");
+const usrInput2 = document.getElementById("usr-input-2");
+const usrInput3 = document.getElementById("usr-input-1");
+
+const containerElement = document.getElementById("container");
+
+containerElement.style.backgroundColor = 
